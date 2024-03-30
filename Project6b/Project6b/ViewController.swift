11d9c0820d8c0378d30be91239ad1698479a367f
thee-falcon <omar.makran.10@gmail.com>
2024-03-30 18:29:05 +0000

    Auto Layout metrics and priorities

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
             This gives you more control over the layout by allowing you to define your own constraints or use Auto Layout without conflicts.
          */
         label1.translatesAutoresizingMaskIntoConstraints = false
-        // set the backgraound color.
+        // set the backgraound color of the label.
         label1.backgroundColor = .red
         // this text will be display inside the label.
         label1.text = "THESE"
@@ -63,10 +63,32 @@ class ViewController: UIViewController {
         let viewDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
         
         for label in viewDictionary.keys {
+            /*
+             NSLayoutConstraint.constraints: A Class Method of NSLayoutConstraint, used to creat an array of constraints based on a visual format.
+             "H:":                           Specifies that the constraints being defined are horizontal.
+             "|":                            Represents the leading (left) and trailing (right) edges of the superview.
+             [\(label)]:                     Substitutes the label identifier from the visual format dictionary (viewDictionary) into the format string.
+             options: []                     An empty set of options, meaning no special options are set for these constraints.
+             */
             view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewDictionary))
         }
         
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewDictionary))
+        // (==88): set a specific height constraint for each of the labels, but we can do it in another way, using "metrics".
+        // "-" a default spacing.
+        // -(>=10)-|: Represents a minimum spacing of 10 points to the bottom edge of the superview.
+
+        /*
+         The @999 attached to the label1 height constraint indicates its priority level. In Auto Layout, constraints can have priority values ranging from 1 to 1000, where a higher value indicates a stronger priority. The default priority for constraints is 1000.
+         
+         In this context, a priority of 999 means that this constraint is very important but not required. If there were conflicting constraints, those with a higher priority value would be satisfied first.
+         
+         the height constraint of label1 with a priority of 999 suggests that while it's crucial to maintain this constraint, it can be broken if necessary to satisfy other higher-priority constraints.
+         */
+        
+        // let's use the "metrics" to avoid the hard coding => (==88).
+        let metrics = ["lableHeight": 88]
+
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(lableHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewDictionary))
     }
 
 
